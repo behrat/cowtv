22c7ccb20cc7ecc672872ae9248667e72649145e
Braden Ehrat <github@bradenehrat.com>
2013-11-10 19:55:34 -0600

    More changes

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 install:
 	mkdir -p /etc/cowtv
-	cp cowtv.yml /etc/cowtv/
+	cp config/* /etc/cowtv/
 	cp cowtv.py /usr/local/bin/cowtv
 	chmod +x /usr/local/bin/cowtv
--- cowtv.py
+++ cowtv.py
@@ -1,32 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time 
 import yaml
 import pylirc
 import select
 import logging
 import subprocess
 from socketIO_client import SocketIO
+
 FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(format=FORMAT)
 
 log = logging.getLogger("CowHD")
 log.setLevel(logging.INFO)
 
-socketIO = SocketIO("localhost", 8888)
+config_file = open('/etc/cowtv/cowtv.yml')
+cowtv_config = yaml.load(config_file)
+config_file.close()
+
+socketIO = None
 
 def dc_lights_update(status):
     log.info("dc_lights_update: " + str(status))
-
-socketIO.on('dc_lights', dc_lights_update)
-
 def keep_alive():
     log.debug("Received keep_alive")
 
+socketIO = SocketIO(cowtv_config["weblights"], 8888)
+socketIO.on('dc_lights', dc_lights_update)
 socketIO.on("keep_alive", keep_alive)
 
 class CowHdController(object):
 
-    def __init__(self, config):
+    def __init__(self, cameras_config):
         self.cameras = []
-        for camera_config in config:
+        for camera_config in cameras_config:
             self.cameras.append(CameraView(**camera_config))
 
     def show_all_cameras(self):
@@ -49,10 +58,13 @@ class CowHdController(object):
             return
         self.stop()
         self.cameras[number].start_full()
+        
 
     def stop(self):
         for camera in self.cameras:
             camera.stop()
+        time.sleep(1)
+        os.system("killall omxplayer.bin");
 
 class CameraView(object):
     width = 1920
@@ -90,49 +102,64 @@ class CameraView(object):
         try:
             self.player.stdin.write('q')
         except IOError, e:
-            self.log.warn(str(e))
+            log.warn(str(e))
         self.player = None
 
 def main():
     log.info("Starting")
-    config_file = open('cameras.yml')
-    cameras_config = yaml.load(config_file)
-    config_file.close()
-    
-    controller = CowHdController(cameras_config)
+
+    controller = CowHdController(cowtv_config["cameras"])
     
 #    Start wih all cameras
     controller.show_all_cameras()
 
-    pylirc_socket = pylirc.init("cowtv", "~/.lircrc", False)
+    pylirc_socket = pylirc.init("cowtv", "/etc/cowtv/lircrc", False)
     while(True):
-        select.select([pylirc_socket],[],[])
-        codes = pylirc.nextcode(1)
-        #if not codes:
-        #    log.warn("None code")
-        #    continue
-        log.info("Got codes: " + str(codes))
-        if not codes:
-            continue
-        for code in codes:
-            command = code["config"]
-            log.info("Got command: " + str(command))
-            if command == "SELECT":
-                log.info("Toggling Lights")
-                socketIO.emit("toggle_lights")
-#            elif command == "TOGGLE_LIGHTS":
-#                socketIO.emit("toggle_lights")
-            elif command == "CLEAR":
-                controller.stop()
-            else:
-                try:
-                    cam_number = int(command)
-                except ValueError:
-                    log.warn("Unreconized IR command: " + command)
+        (iready, oready, eready) = select.select([sys.stdin, pylirc_socket],[],[])
+        for s in iready:
+            if s == sys.stdin:
+                line = sys.stdin.readline()
+                line = line.strip()
+                if line == 't':
+                    log.info("Toggling Lights")
+                    socketIO.emit("toggle_lights")
                 else:
-                    if cam_number == 0:
-                        controller.show_all_cameras()
+                    try:
+                        cam_number = int(line)
+                    except ValueError:
+                        log.warn("Unreconized command: " + line)
                     else:
-                        controller.show_camera(cam_number)
+                        if cam_number == 0:
+                            controller.show_all_cameras()
+                        else:
+                            controller.show_camera(cam_number)
+            elif s == pylirc_socket:
+                codes = pylirc.nextcode(1)
+                #if not codes:
+                #    log.warn("None code")
+                #    continue
+                log.info("Got codes: " + str(codes))
+                if not codes:
+                    continue
+                for code in codes:
+                    command = code["config"]
+                    log.info("Got command: " + str(command))
+                    if command == "SELECT":
+                        log.info("Toggling Lights")
+                        socketIO.emit("toggle_lights")
+#                    elif command == "TOGGLE_LIGHTS":
+#                        socketIO.emit("toggle_lights")
+                    elif command == "CLEAR":
+                        controller.stop()
+                    else:
+                        try:
+                            cam_number = int(command)
+                        except ValueError:
+                            log.warn("Unreconized IR command: " + command)
+                        else:
+                            if cam_number == 0:
+                                controller.show_all_cameras()
+                            else:
+                                controller.show_camera(int(command))
 
 main()
